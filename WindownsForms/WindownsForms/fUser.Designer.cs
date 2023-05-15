a015b9bf002783e79be7c2b1dad44fcddb71861a
Hoai1921 <121252697+Hoai1921@users.noreply.github.com>
2023-05-15 14:41:23 +0700

    commit 4

---
+++ WindownsForms/WindownsForms/fUser.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindownsForms
+{
+    partial class fUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fUser));
+            this.tbDanhSach = new System.Windows.Forms.TabPage();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dgvDanhSach = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.tcAdmin = new System.Windows.Forms.TabControl();
+            this.tbDanhSach.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.tcAdmin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbDanhSach
+            // 
+            this.tbDanhSach.Controls.Add(this.panel4);
+            this.tbDanhSach.Controls.Add(this.panel3);
+            this.tbDanhSach.Location = new System.Drawing.Point(4, 25);
+            this.tbDanhSach.Name = "tbDanhSach";
+            this.tbDanhSach.Padding = new System.Windows.Forms.Padding(3);
+            this.tbDanhSach.Size = new System.Drawing.Size(1053, 172);
+            this.tbDanhSach.TabIndex = 2;
+            this.tbDanhSach.Text = "Danh sách Folder";
+            this.tbDanhSach.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dgvDanhSach);
+            this.panel4.Location = new System.Drawing.Point(6, 58);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1041, 108);
+            this.panel4.TabIndex = 1;
+            // 
+            // dgvDanhSach
+            // 
+            this.dgvDanhSach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDanhSach.Location = new System.Drawing.Point(0, -3);
+            this.dgvDanhSach.Name = "dgvDanhSach";
+            this.dgvDanhSach.RowHeadersWidth = 51;
+            this.dgvDanhSach.RowTemplate.Height = 24;
+            this.dgvDanhSach.Size = new System.Drawing.Size(1038, 105);
+            this.dgvDanhSach.TabIndex = 0;
+            this.dgvDanhSach.DoubleClick += new System.EventHandler(this.dgvDanhSach_DoubleClick);
+            this.dgvDanhSach.MouseUp += new System.Windows.Forms.MouseEventHandler(this.dgvDanhSach_MouseUp);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.txtSearch);
+            this.panel3.Location = new System.Drawing.Point(6, 6);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1041, 46);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
+            this.btnSearch.Location = new System.Drawing.Point(881, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(157, 40);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Tìm Kiếm";
+            this.btnSearch.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSearch.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(513, 10);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(362, 27);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // tcAdmin
+            // 
+            this.tcAdmin.Controls.Add(this.tbDanhSach);
+            this.tcAdmin.Location = new System.Drawing.Point(10, 10);
+            this.tcAdmin.Name = "tcAdmin";
+            this.tcAdmin.SelectedIndex = 0;
+            this.tcAdmin.Size = new System.Drawing.Size(1061, 201);
+            this.tcAdmin.TabIndex = 1;
+            // 
+            // fUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 220);
+            this.Controls.Add(this.tcAdmin);
+            this.MaximizeBox = false;
+            this.Name = "fUser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản Lý Bảng Điểm";
+            this.Load += new System.EventHandler(this.fUser_Load);
+            this.tbDanhSach.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDanhSach)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.tcAdmin.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tbDanhSach;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.DataGridView dgvDanhSach;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TabControl tcAdmin;
+    }
+}
